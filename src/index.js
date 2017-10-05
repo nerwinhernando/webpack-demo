345bb9d6accfcd0c0b6db333c735b2ac34ce3079
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-10-05 23:46:56 +0800

    Environments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
-import printMe from './print.js';
-import './style.css';
 import { cube } from './math.js';
+
+if(process.env.NODE_ENV !== 'production') {
+	console.log('looks like we are in development mode!');
+}
 
 function component() {
   var element = document.createElement('pre');
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
@@ -5,9 +6,12 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
    devtool: 'source-map',
    plugins: [
-    new UglifyJSPlugin({
-    	sourceMap: true
-    }),
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+    	'process.env': {
+    		'NODE_ENV': JSON.stringify('production')
+    	}
+    })
   ],
      module: {
      rules: [
